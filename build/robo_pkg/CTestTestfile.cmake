a575c1cca024245e0d7255e785d68452f571cef2
Ayush <ay.chaudhary@jacobs-university.de>
2020-12-25 03:55:32 +0100

    First commit

---
+++ build/robo_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ayush/catkinn_ws/src/robo_pkg
+# Build directory: /home/ayush/catkinn_ws/build/robo_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
